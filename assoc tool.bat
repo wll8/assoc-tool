a6bc85e5aedaf6f91b8e8e990b5649b3fc03249b
wll8 <xwdaysme@gmail.com>
2018-01-27 09:59:39 +0800

    init

--- assoc tool.bat
+++ assoc tool.bat
@@ -1,22 +1,23 @@
-@echo off&color 2f&mode con lines=45 cols=145&title 文件关联工具
+@echo off&color f2&mode con lines=45 cols=145&title 文件关联工具 -- by xw
 
 echo.
 echo.-------------- 文件关联工具使用说明 --------------
 echo.
 echo 操作序号：
 echo   1: 添加右键菜单
-echo   2: 删除右键菜单
-echo   3: 关联扩展名(与程序同目录的 ext.txt 文件中)
+echo   2: 关联扩展名(与程序同目录的 ext.txt 文件中)
+echo   3: 删除右键菜单
 echo   4: 删除扩展名
 echo   5: 退出
 echo.
 
 echo 拖入程序后，点击此窗口按 enter 键：
 set /p exe_path=
-for %%i in (%exe_path%) do (set file_name=%%~nxi)
-for %%i in (%exe_path%) do (set file_path=%%~dpi)
+for %%i in (%exe_path%) do (set exe_name=%%~nxi)
+for %%i in (%exe_path%) do (set temp=%%~dpi)
+set exe_name=%exe_name:.exe=%
 set exe_path=%exe_path:"=%
-set ext_path=%file_path%ext.txt
+set ext_path=%temp%ext.txt
 if not exist "%ext_path%" (echo.&echo 注意：&echo 扩展名列表文件 "%ext_path%" 不存在。&pause>nul&exit)
 
 
@@ -24,20 +25,20 @@ if not exist "%ext_path%" (echo.&echo 注意：&echo 扩展名列表文件 "%ext_path%" 不
 echo.
 set u=&set /p u=输入操作序号：
 if "%u%" == "1" goto reg_menu
-if "%u%" == "2" goto un_reg_menu
-if "%u%" == "3" goto text_file
+if "%u%" == "2" goto text_file
+if "%u%" == "3" goto un_reg_menu
 if "%u%" == "4" goto un_text_file
 if "%u%" == "5" exit
 goto begin
 
 
 :reg_menu
-reg add "hkcr\*\shell\%file_name%" /ve /d "使用 %file_name% 打开" /f >nul 2>nul
-reg add "hkcr\*\shell\%file_name%\command" /ve /d "%exe_path% """%%1%"""" /f >nul 2>nul
+reg add "hkcr\*\shell\%exe_name%" /ve /d "使用 %exe_name% 打开" /f >nul 2>nul
+reg add "hkcr\*\shell\%exe_name%\command" /ve /d "%exe_path% """%%1%"""" /f >nul 2>nul
 echo.&echo 注册右键菜单完成 &echo.&goto begin
 
 :un_reg_menu
-reg delete "hkcr\*\shell\%file_name%" /f >nul 2>nul
+reg delete "hkcr\*\shell\%exe_name%" /f >nul 2>nul
 echo.&echo 卸载右键菜单完成 &echo.&goto begin
 
 :text_file
